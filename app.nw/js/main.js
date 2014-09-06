b0a783df270759f1a50c5be21f0a7fac5d75e980
nanjixiong218 <695540791@qq.com>
2014-09-07 00:08:07 +0800

    用真正的异步实现循环遍历文件重命名，体验了一下恶魔金字塔！

--- app.nw/js/main.js
+++ app.nw/js/main.js
@@ -28,12 +28,12 @@
              alert("ok");
         }
         */
-        //等价的异步操作,顺序执行，注意重名覆盖问题
+        /*等价的异步操作,顺序执行，注意重名覆盖问题，这种异步其实和同步无异，并没有真正的利用异步的特性
         fs.stat(dirPath,function (err,status) {
             if(status.isDirectory()){
                 fs.readdir(dirPath,function(err,files){
 
-                    (function next(i){
+                    (function next(i){//先重命名为当前时间戳
                         if(i<files.length){
                             console.log(files.length);
                             console.log(i);
@@ -47,7 +47,8 @@
                                 }
                             });
                         }else{
-                            fs.readdir(dirPath,function(err,files){
+                            //这里是所有异步结束之后的操作
+                            fs.readdir(dirPath,function(err,files){//再重命名为特定名字
                                 (function next(i){
                                     if(i<files.length) {
                                         console.log(files.length);
@@ -62,6 +63,7 @@
                                             }
                                         });
                                     }else{
+
                                         alert("ok");
                                     }
                                 })(0);
@@ -73,7 +75,67 @@
                 });
             }
         })
+        */
+        //这种异步才是真正的利用了异步，注意如果直接用时间戳会出现重名问题，
+        // 同一时间戳可能已经完成数个文件的重命名，所以用到了时间戳加序号的方式
+        //但是大家已经看出这种代码是多么的难看了！
+        fs.stat(dirPath,function (err,status) {
+            if(status.isDirectory()){
+                fs.readdir(dirPath,function(err,files){
+                    var times = 0;
+                    (function next(i){//先重命名为当前时间戳
+                        if(i<files.length){
+                            console.log(files.length);
+                            console.log(i);
+                            var oldName = path.join(dirPath,files[i]);
+                            var newName = path.join(dirPath,Date.now()+i+".jpg");
+                            fs.rename(oldName,newName,function(err){
+                                if(err){
+                                    console.log(err);
+                                }else{
 
+                                   if(times==files.length-1){
+                                       //这里是所有异步接受之后的操作
+
+                                       fs.readdir(dirPath,function(err,files){
+                                           var times = 0 ;
+                                           (function next(i){
+                                               if(i<files.length){
+                                                   var oldName = path.join(dirPath,files[i]);
+                                                   var newName = path.join(dirPath,"xu"+i+".jpg");
+                                                   fs.rename(oldName,newName,function(err){
+                                                       if(err){
+                                                           console.log(err);
+                                                       }else{
+                                                           if(times==files.length-1){
+                                                               alert("over!");
+                                                           }else{
+                                                               times++;
+                                                           }
+
+                                                       }
+                                                   });
+                                                   next(++i);
+                                               }else{
+                                                   alert("异步2启动完毕!");
+                                               }
+                                           })(0)
+                                       });
+                                   }else{
+                                       times++;
+                                   }
+
+                                }
+                            });
+                            next(++i);
+                        }else{
+                            alert("异步1启动完毕!");
+                        }
+                    }(0));
+
+                });
+            }
+        })
     });
 };
 window.onload = loaded;
