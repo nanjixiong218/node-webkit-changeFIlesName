6428b3cbb0ccb49fb0474f53762973e9558b7d7f
nanjixiong218 <695540791@qq.com>
2014-09-06 14:46:01 +0800

    seajs和node-webkit的模块冲突，seajs覆盖了nodejs的require

--- app.nw/js/main.js
+++ app.nw/js/main.js
@@ -2,13 +2,32 @@
  * Created by xu on 2014/9/5.
  */
 define(function(require,exports,module){
-    var $ = require("$");
-    var chooseFile = $("#chooseFile");
-    var chooseExt = $(":checkbox[name='changedExt']").val();
-    var fileNamePre = $(":text[name='fileNamePre']").val();
-    var fileNameExt = $(":radio[name='fileNameExt']").val();
-    alert(fileNameExt);
+    var fs = require("fs");
+    var path = require("path");
     exports.init = function(){
-
+        var $ = require("$");
+        var chooseFile = $("#chooseFile");
+        var chooseExt = $(":checkbox[name='changedExt']");
+        var fileNamePre = $(":text[name='fileNamePre']");
+        var fileNameExt = $(":radio[name='fileNameExt']");
+        var beginBtn = $("#begin");
+        var dirPath;
+        chooseFile.on("change", function () {
+            dirPath = this.value.replace(/(.*\\).*$/,"$1");
+            alert(dirPath);
+        });
+        beginBtn.on("click",function(){
+            if(fs.statSync(dirPath).isDirectory()){
+                alert(1);
+                /*
+                fs.readdirSync(dirPath).forEach(function(file,i){
+                    var oldName = path.join(dirPath,file);
+                    var newName = path.join(dirPath,"xu"+i+".jpg");
+                    fs.renameSync(oldName,newName);
+                });
+                alert("ok");
+                */
+            }
+        });
     }
 });
