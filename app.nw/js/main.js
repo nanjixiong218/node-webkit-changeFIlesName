f763c424b8473c7411aaf1e4bd10818db608fbf8
nanjixiong218 <695540791@qq.com>
2014-09-08 22:12:45 +0800

    jquery选择器还是不熟

--- app.nw/js/main.js
+++ app.nw/js/main.js
@@ -14,16 +14,22 @@
 
     chooseFile.on("change", function () {
         dirPath = this.value.replace(/(.*\\).*$/, "$1");
-        alert(dirPath);
+        //alert(dirPath);
     });
-    beginBtn.on("click", function () {
 
-        /*同步操作
+    beginBtn.on("click", function () {
+        /*
+        //同步操作
         if (fs.statSync(dirPath).isDirectory()) {
              fs.readdirSync(dirPath).forEach(function(file,i){
-             var oldName = path.join(dirPath,file);
-             var newName = path.join(dirPath,"xu"+i+".jpg");
-             fs.renameSync(oldName,newName);
+                var oldName = path.join(dirPath,file);
+                var newName = path.join(dirPath,Date.now()+i+".jpg");
+                fs.renameSync(oldName,newName);
+             });
+             fs.readdirSync(dirPath).forEach(function(file,i){
+                var oldName = path.join(dirPath,file);
+                var newName = path.join(dirPath,"xu"+i+".jpg");
+                fs.renameSync(oldName,newName);
              });
              alert("ok");
         }
@@ -38,7 +44,7 @@
                             console.log(files.length);
                             console.log(i);
                             var oldName = path.join(dirPath,files[i]);
-                            var newName = path.join(dirPath,Date.now()+".jpg");
+                            var newName = path.join(dirPath,Date.now()+i+".jpg");
                             fs.rename(oldName,newName,function(err){
                                 if(err){
                                     console.log(err);
@@ -80,7 +86,7 @@
         //这种异步才是真正的利用了异步，注意如果直接用时间戳会出现重名问题，
         // 同一时间戳可能已经完成数个文件的重命名，所以用到了时间戳加序号的方式
         //但是大家已经看出这种代码是多么的难看了！
-        //这种用IIFE闭包的方式可以替换为forEach吧。
+
         fs.stat(dirPath,function (err,status) {
             if(status.isDirectory()){
                 fs.readdir(dirPath,function(err,files){
@@ -139,7 +145,7 @@
             }
         });
         */
-        /*forEach改写next闭包
+        //forEach改写next IIFE形成第一版
         fs.stat(dirPath,function (err,status) {
             if(status.isDirectory()){
                 fs.readdir(dirPath,function(err,files){
@@ -147,43 +153,66 @@
                     files.forEach(function(file,i){
                             console.log(files.length);
                             console.log(i);
-                            var oldName = path.join(dirPath,file);
-                            var newName = path.join(dirPath,Date.now()+i+".jpg");
-                            fs.rename(oldName,newName,function(err){
-                                if(err){
-                                    console.log(err);
-                                }else{
-
-                                    if(times==files.length-1){
-                                        //这里是所有异步接受之后的操作
-                                        fs.readdir(dirPath,function(err,files){
-                                            var times = 0 ;
-                                            files.forEach(function(file,i){
-                                                    var oldName = path.join(dirPath,file);
-                                                    var newName = path.join(dirPath,"xu"+i+".jpg");
-                                                    fs.rename(oldName,newName,function(err){
-                                                        if(err){
-                                                            console.log(err);
-                                                        }else{
-                                                            if(times==files.length-1){
-                                                                alert("over!");
-                                                            }else{
-                                                                times++;
-                                                            }
-                                                        }
-                                                    });
-                                            });
-                                        });
+                            if(file.indexOf("."+chooseExt.val())!=-1){
+                                var oldName = path.join(dirPath,file);
+                                var newName = path.join(dirPath,Date.now()+i+".jpg");
+                                fs.rename(oldName,newName,function(err){
+                                    if(err){
+                                        console.log(err);
                                     }else{
-                                        times++;
+
+                                        if(times==files.length-1){
+                                            //这里是所有异步接受之后的操作
+
+                                            fs.readdir(dirPath,function(err,files){
+                                                var times = 0 ;
+                                                files.forEach(function(file,i){
+                                                    if(file.indexOf("."+chooseExt.val())!=-1){
+                                                        var oldName = path.join(dirPath,file);
+                                                        var newName = path.join(dirPath,fileNamePre.val()+i+"."+fileNameExt.val());
+                                                        fs.rename(oldName,newName,function(err){
+                                                            if(err){
+                                                                console.log(err);
+                                                            }else{
+                                                                if(times==files.length-1){
+                                                                    alert("over!");
+                                                                }else{
+                                                                    times++;
+                                                                }
+                                                            }
+                                                        });
+                                                    }else{
+                                                        if(times==files.length-1){
+                                                            alert("over!");
+                                                        }else{
+                                                            times++;
+                                                        }
+                                                    }
+
+                                                });
+                                            });
+                                        }else{
+                                            times++;
+                                        }
                                     }
+                                });
+                            }else{
+                                if(times==files.length-1){
+                                    alert("over!");
+                                }else{
+                                    times++;
                                 }
-                            });
+                            }
+
+
                     });
                 });
             }
         })
-        */
+
+        //最佳方案，用promise或者用一些流行异步解决方案库，比如proxy，async，when,then等
+        //这里选择whenjs
+
     });
 };
 window.onload = loaded;
